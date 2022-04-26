99af175a91e0ed92695129f544efb3371af40be1
tncrdk <85764203+tncrdk@users.noreply.github.com>
2022-04-26 16:48:11 +0200

    filstruktur oppdatering

--- src/lib_path.py
+++ src/lib_path.py
@@ -1,7 +1,7 @@
 import sys
 
 paths_to_include = [
-    r"C:\Users\thorb\Documents\Python\TekForsk\Idioten Optimalisert\GameEngine"
+    r"C:\Users\thorb\Documents\Python\TekForsk\Idioten Optimalisert\Lib"
 ]
 
 sys.path += paths_to_include
